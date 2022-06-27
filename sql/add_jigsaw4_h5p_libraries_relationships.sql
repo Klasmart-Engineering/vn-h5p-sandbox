645c2ab0fe393548e44ccdb8f6668314dabb8359
Cang Ta <cang.ta@kidsloop.live>
2022-06-27 11:02:26 +0700

    fix: jigsaw h5p library cannot load the content

---
+++ sql/add_jigsaw4_h5p_libraries_relationships.sql
@@ -0,0 +1,7 @@
+INSERT INTO `wp_h5p_libraries_libraries` (`library_id`, `required_library_id`, `dependency_type`) VALUES
+(74, 38, 'preloaded'),
+(74, 6, 'preloaded'),
+(74, 14, 'preloaded'),
+(74, 90, 'preloaded'),
+(74, 50, 'editor'),
+(74, 9, 'editor');
---
+++ sql/add_soundjs_h5p_library.sql
@@ -0,0 +1,2 @@
+INSERT INTO wp_h5p_libraries(created_at, updated_at, name, title, major_version, minor_version, patch_version, preloaded_js, runnable, fullscreen, embed_types, semantics, tutorial_url) VALUES(NOW(), NOW(), 'H5P.SoundJS', 'Sound js api', 1, 0, 2, 'soundjs-0.6.2.mi
+n.js', 0, 0, '', '', '');
